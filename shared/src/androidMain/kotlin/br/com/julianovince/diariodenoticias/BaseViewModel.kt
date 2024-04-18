6326f825203d3d4d3385b4f9c9958f6a592d20dc
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-18 09:08:28 -0300

    Classe Arquitetura base do Android onde temos o viewModelScope, conseguimos controlar as Coroutines vinculadas ao viewModel e seu ciclo de vida.

---
+++ shared/src/androidMain/kotlin/br/com/julianovince/diariodenoticias/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package br.com.julianovince.diariodenoticias
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+
+actual open class BaseViewModel : ViewModel() {
+
+    actual val scope = viewModelScope
+
+}
