d0846c54526c526dd2b72ca6906ff63cf9d589e2
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-19 10:47:25 -0300

    Inicializar Koin para injeção de dependência
    Este commit adiciona a inicialização do Koin para a injeção de dependência na classe ArticlesInjector(iOS), permitindo o acesso ao ArticlesViewModel por meio da injeção.

---
+++ shared/src/iosMain/kotlin/br/com/julianovince/diariodenoticias/di/KoinInitializer.kt
@@ -0,0 +1,20 @@
+package br.com.julianovince.diariodenoticias.di
+
+import br.com.julianovince.diariodenoticias.articles.ArticlesViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.context.startKoin
+
+fun initKoin() {
+
+    val modules = sharedKoinModule
+
+    startKoin {
+        modules(modules)
+    }
+
+    class ArticlesInjector : KoinComponent {
+        val articlesViewModel: ArticlesViewModel by inject()
+    }
+
+}
