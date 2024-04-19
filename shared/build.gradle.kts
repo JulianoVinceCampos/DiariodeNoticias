af011185f79e741469903268789d3ba11a87c53a
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-19 07:34:25 -0300

    Adiciona a serialização Kotlin ao plugin Kotlin, dependências do Ktor e Koin, tanto para Android quanto para iOS.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     id("co.touchlab.skie") version "0.4.19"
+    kotlin("plugin.serialization") version "1.9.20"
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
     targetHierarchy.default()
+
     androidTarget {
         compilations.all {
             kotlinOptions {
@@ -22,42 +24,47 @@ kotlin {
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
-            isStatic = true
         }
     }
 
     sourceSets {
-        commonMain.dependencies {
-            implementation(libs.kotlinx.coroutines.core)
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.kotlinx.datetime)
+                implementation(libs.koin.core)
+            }
         }
 
         val androidMain by getting {
             dependencies {
                 implementation(libs.androidx.lifecycle.viewmodel.ktx)
+                implementation(libs.ktor.client.android)
             }
         }
 
         val iosMain by getting {
             dependencies {
-
+                implementation(libs.ktor.client.darwin)
             }
         }
 
-
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
         }
     }
 }
+
 
 android {
     namespace = "br.com.julianovince.diariodenoticias"
     compileSdk = 34
     defaultConfig {
         minSdk = 24
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
