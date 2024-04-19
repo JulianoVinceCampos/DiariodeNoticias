be754d71ad254470e01e8fe1a9343915767c6818
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-19 07:29:47 -0300

    Adiciona a classe de dados ArticlesResponse para representar a resposta de uma solicitação de artigos.

---
+++ shared/src/commonMain/kotlin/br/com/julianovince/diariodenoticias/articles/ArticlesResponse.kt
@@ -0,0 +1,14 @@
+package br.com.julianovince.diariodenoticias.articles
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ArticlesResponse (
+    @SerialName("status")
+    val status:String,
+    @SerialName("totalResults")
+    val results:Int,
+    @SerialName("articles")
+    val articles: List<ArticleRaw>
+)
