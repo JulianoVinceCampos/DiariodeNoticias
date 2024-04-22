7a99f5182e7d7fc9115b53f9af6a3083c6100ff7
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-22 16:41:41 -0300

    Atualização: Atualizados imports e dependências do módulo de artigos para refletir a nova estrutura.
    Adição: Adicionadas dependências para ArticlesDataSource e ArticlesRepository no módulo de artigos.

--- shared/src/commonMain/kotlin/br/com/julianovince/diariodenoticias/articles/di/ArticlesModule.kt
+++ shared/src/commonMain/kotlin/br/com/julianovince/diariodenoticias/articles/di/ArticlesModule.kt
@@ -1,6 +1,8 @@
-import br.com.julianovince.diariodenoticias.articles.ArticlesUseCase
-import br.com.julianovince.diariodenoticias.articles.ArticlesService
-import br.com.julianovince.diariodenoticias.articles.ArticlesViewModel
+import br.com.julianovince.diariodenoticias.articles.data.ArticlesRepository
+import br.com.julianovince.diariodenoticias.articles.data.ArticlesDataSource
+import br.com.julianovince.diariodenoticias.articles.application.ArticlesUseCase
+import br.com.julianovince.diariodenoticias.articles.data.ArticlesService
+import br.com.julianovince.diariodenoticias.articles.presentation.ArticlesViewModel
 import org.koin.dsl.module
 
 
@@ -9,5 +11,7 @@ val articlesModule = module {
     single<ArticlesService> { ArticlesService(get()) }
     single<ArticlesUseCase> { ArticlesUseCase(get()) }
     single<ArticlesViewModel> { ArticlesViewModel(get()) }
+    single<ArticlesDataSource> { ArticlesDataSource(get()) }
+    single<ArticlesRepository> { ArticlesRepository(get(), get()) }
 
 }
