077b2efaa02040eac674cec75bf6b0c31a20b603
JULIANOCAMPOS\julia <julianovincedecampos@hotmail.com>
2024-04-22 16:44:00 -0300

    Remoção: Removida a classe ArticlesState do pacote de artigos, pois não é mais necessária.

--- shared/src/commonMain/kotlin/br/com/julianovince/diariodenoticias/articles/ArticlesState.kt
+++
@@ -1,7 +0,0 @@
-package br.com.julianovince.diariodenoticias.articles
-
-data class ArticlesState (
-    val articles: List<Article> = listOf(),
-    val loading: Boolean = false,
-    val error:String? = null
-)
